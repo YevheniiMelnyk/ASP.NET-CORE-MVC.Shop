7379387b7752b3e230db3c66c50f12fd214baf22
YevheniiMelnyk <melnik.zheka@gmail.com>
2023-05-19 14:08:02 +0300

    CRUD operations for categories

--- Shop/Shop/Controllers/CategoryController.cs
+++ Shop/Shop/Controllers/CategoryController.cs
@@ -29,7 +29,73 @@ namespace Shop.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Add(Category category)
         {
-            _dbContext.Category.Add(category);
+            if (ModelState.IsValid)
+            {
+                _dbContext.Category.Add(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = _dbContext.Category.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Category.Update(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = _dbContext.Category.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _dbContext.Category.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Category.Remove(category);
             _dbContext.SaveChanges();
             return RedirectToAction("Index");
         }
--- Shop/Shop/Models/Category.cs
+++ Shop/Shop/Models/Category.cs
@@ -7,9 +7,14 @@ namespace Shop.Models
     {
         [Key]
         public int CategoryId { get; set; }
+
+        [Required]
         [DisplayName("Category Name")]
         public string CategoryName { get; set; }
+
+        [Required]
         [DisplayName("Display Order")]
+        [Range(1, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
         public int DisplayOrder{ get; set; }
     }
 }
