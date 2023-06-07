b8c1ef0501f9face295666c809a314472f12360e
YevheniiMelnyk <melnik.zheka@gmail.com>
2023-06-07 16:12:43 +0300

    add to cart / remove from cart

--- Shop/Shop/Controllers/HomeController.cs
+++ Shop/Shop/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Shop.Data;
 using Shop.Models;
 using Shop.Models.ViewModels;
+using Shop.Utility;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -36,6 +37,12 @@ namespace Shop.Controllers
 
         public IActionResult Details(int id)
         {
+            List<Cart> cartList = new List<Cart>();
+            if (HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart) != null && HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                cartList = HttpContext.Session.Get<List<Cart>>(WebConstants.SessionCart);
+            }
+
             DetailsVM  detailsVM = new()
             {
                 Product = _dbContext.Product.Include(u => u.Category)
@@ -43,7 +50,50 @@ namespace Shop.Controllers
                 ExistsInCart = false
             };
 
+            foreach(var item in cartList)
+            {
+                if(item.ProductId == id)
+                {
+                    detailsVM.ExistsInCart = true;
+                }
+            }
+
             return View(detailsVM);
+        }
+
+        [HttpPost, ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+            List<Cart> cartList = new List<Cart>();
+
+            if(HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart) != null && HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                cartList = HttpContext.Session.Get<List<Cart>>(WebConstants.SessionCart);
+            }
+
+            cartList.Add(new Cart { ProductId = id });
+            HttpContext.Session.Set(WebConstants.SessionCart, cartList);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            List<Cart> cartList = new List<Cart>();
+
+            if(HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart) != null && HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                cartList = HttpContext.Session.Get<List<Cart>>(WebConstants.SessionCart);
+            }
+
+            var itemToRemove = cartList.SingleOrDefault(i => i.ProductId == id);
+            if(itemToRemove != null)
+            {
+                cartList.Remove(itemToRemove);
+            }
+
+            HttpContext.Session.Set(WebConstants.SessionCart, cartList);
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
--- Shop/Shop/WebConstants.cs
+++ Shop/Shop/WebConstants.cs
@@ -3,5 +3,6 @@
     public static class WebConstants
     {
         public static string ImagePath = @"\Images\Product\";
+        public static string SessionCart = "ShoppingCartSession";
     }
 }
