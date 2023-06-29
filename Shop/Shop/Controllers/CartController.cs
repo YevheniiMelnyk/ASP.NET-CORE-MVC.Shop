58e1389141626c171b9d1469a00705daf47334aa
YevheniiMelnyk <melnik.zheka@gmail.com>
2023-06-29 13:24:32 +0300

    shopping cart

---
+++ Shop/Shop/Controllers/CartController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models;
+using Shop.Models.ViewModels;
+using Shop.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Shop.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Cart> shoppingCartList = new();
+            if (HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart) != null && HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<Cart>>(WebConstants.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+
+            IEnumerable<Product> prodList = _context.Product.Where(p => prodInCart.Contains(p.Id));
+
+            return View(prodList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            List<Cart> shoppingCartList = new();
+            if (HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart) != null && HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<Cart>>(WebConstants.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _context.Product.Where(p => prodInCart.Contains(p.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _context.AppUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = prodList
+            };
+
+            return View(ProductUserVM);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<Cart> shoppingCartList = new();
+            if(HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart) != null && HttpContext.Session.Get<IEnumerable<Cart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<Cart>>(WebConstants.SessionCart);
+            }
+
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(i => i.ProductId == id));
+
+            HttpContext.Session.Set(WebConstants.SessionCart, shoppingCartList);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Shop/Shop/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Shop.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM() 
+        {
+            ProductList = new List<Product>();
+        }
+
+        public AppUser ApplicationUser { get; set; }
+
+        public IEnumerable<Product> ProductList { get; set; }
+    }
+}
